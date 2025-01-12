6e47644cd1c1407043b9fadced7d731518140bda
Saumitra Bhanage <bhanage.saumitra@gmail.com>
2025-01-11 22:17:19 -0800

    Huggingface streamlit support

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import requests
 
 # Read default job description and resume
 try:
-    with open('../resources/sample_job_description.txt', 'r') as file:
+    with open('resources/sample_job_description.txt', 'r') as file:
         default_job_description = file.read()
 except FileNotFoundError:
     default_job_description = "Paste the job description here."
 
 try:
-    with open('../resources/sample_resume.txt', 'r') as file:
+    with open('resources/sample_resume.txt', 'r') as file:
         default_resume_content = file.read()
 except FileNotFoundError:
     default_resume_content = "Your resume content here."
@@ -18,19 +18,25 @@ st.markdown("<h1 style='text-align: center;'>Job Hunt Coach</h1>", unsafe_allow_
 
 job_description = default_job_description
 resume_content = default_resume_content
+suggested_questions ="""
+Sample questions to ask the coach:
+1. How can I improve my resume?"
+2. What skills should I add to my resume?
+3. How well does my resume match the job description?
+4. How can I prepare for this interview?
+5. How can I upskill in required skills?
+"""
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-col1, col2 = st.columns(2)
-
 with st.sidebar:
+    st.write("The Coach is here you help you improve your resume and prepare for job interviews")
     job_description = st.text_area("Paste the job description:", job_description, height=300)
     resume_content = st.text_area("Your Resume:", resume_content, height=300)
-
-# with col2:
-st.subheader("Talk to the coach")
+    st.write(suggested_questions)
+    
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
